f56ab063415cc3f8eeb38d2ef3c87de7e4dd9c11
Kenta <kenasuzuki5555@gmail.com>
2021-02-25 03:12:52 +0900

    test: Add code for ch6

---
+++ ch6/src/franc.py
@@ -0,0 +1,11 @@
+from typing import cast
+from money import Money
+
+
+class Franc(Money):
+    def __init__(self, amount: int) -> None:
+        super().__init__(amount)
+
+    def times(self, multiplier: int) -> None:
+        # self.amount *= multiplier
+        return Franc(self._amount * multiplier)
